a0fc8129283e37ed92cc91af74e8ffc5c89b4c73
Vsevolod Goloviznin <vsevolod.goloviznin@gmail.com>
2021-08-18 01:55:49 +0300

    - added cors support

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,12 +1,20 @@
 import express from 'express';
+import cors, { CorsOptions } from 'cors';
 
 import routes from './controllers';
 import { errorMiddleware } from './middlewares/error';
 
 const PORT = process.env.PORT || 3000;
+const allowedOrigins = ['http://localhost:3001'];
+
+const options: CorsOptions = {
+  origin: allowedOrigins,
+};
 
 const app = express();
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors(options));
 app.use(routes);
 
 app.get('/', (_, res) => res.json('OK'));
